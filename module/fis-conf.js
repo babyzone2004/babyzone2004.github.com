05e11a62d1ca3e75c3c78bbb567054835d77e8d9
yangweixian <yangweixian@xiaomi.com>
2016-03-15 17:12:29 +0800

    add carosel.js

---
+++ module/fis-conf.js
@@ -0,0 +1,39 @@
+//把前面的配置都写在这里统一管理
+//项目中就不用再写了
+fis.config.set('roadmap.path', [{
+  reg: 'lib/*.js',
+  isMod: false
+}, {
+  reg: '**/*.js',
+  isMod: true
+}]);
+//插件与配置
+fis.config.merge({
+  roadmap: {
+    domain: 'demo'
+  },
+  modules: {
+    lint: {
+      js: 'jshint',
+      css: 'csslint'
+    },
+    postpackager: ['autoload']
+  },
+  settings: {
+    postprocessor: {
+      jswrapper: {
+        type: 'amd'
+      }
+    },
+    postpackager: {
+      autoload: {
+        useSiteMap: false
+      }
+    },
+    lint: {
+      jshint: {
+        ignored: ['lib/**.js'],
+      }
+    }
+  }
+});
---
+++ module/main.js
@@ -0,0 +1,5 @@
+var carousel = require('/module/carousel/carousel.js');
+
+var myCarousel = carousel({
+  contain: '#J_carousel'
+});
