bc95af31fed89946c3cbde2ac41c47862fd5e843
yangweixian <yangweixian@xiaomi.com>
2016-03-18 22:23:35 +0800

    add tab module

--- module/main.js
+++ module/main.js
@@ -3,3 +3,7 @@ var carousel = require('/module/carousel/carousel.js');
 var myCarousel = carousel({
   contain: '#J_carousel'
 });
+
+
+var Tab = require('/module/tab/tab.js');
+var tab = new Tab();
